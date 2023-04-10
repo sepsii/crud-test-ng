a90dfed4c327be6a28c4267900ecae0a9a017456
sep <74097138+sepsii@users.noreply.github.com>
2023-04-10 12:59:41 +0430

    first commit for test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'crud-test-angular-latest';
+
+
+ngOnInit(): void {
+  console.log('test')
+}
 }
